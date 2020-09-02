578ecab274da42c648b1226c205832317c82df95
Benedict Harcourt <ben.harcourt@harcourtprogramming.co.uk>
2020-09-02 18:33:15 +0000

    Add additional variations to !selfcare

--- src/selfcare/__init__.py
+++ src/selfcare/__init__.py
@@ -10,13 +10,15 @@ import random
 import bot.commands
 
 BIG_REMINDERS = [
-    "take you meds",
+    "take your meds",
     "check your medication",
 ]
 
 REGULAR_THINGS = [
     "eat a meal",
+    "have something to eat",
     "drink some water",
+    "grab a refreshing drink",
     "stretch your body",
     "raise you heart rate",
 ]
@@ -24,20 +26,24 @@ REGULAR_THINGS = [
 DAILY_THINGS = [
     "reach out to someone",
     "tend to a living/growing thing",
-    "take a shower/bath",
+    "take a shower or bath",
     "clean one space or surface",
 ]
 
 FORMATS = [
     "Looking after yourself is key: {reminder}, {regular}, and consider {daily}. 💜",
+    "Looking after yourself is key: {reminder}, {regular}, and consider {daily}. 💜",
+    "Looking after yourself is key: {reminder}, {regular}, and consider {daily}. 💜",
     "Time to {regular}? Maybe {daily}? Also, {reminder}. 🧡",
+    "In the words of dear community member Baron Samedi: meds reminder for those who may need it.",
+    "Remember to {reminder}",
 ]
 
 
 class SelfCare(bot.commands.SimpleCommand):
     """Reminds our dear friends to look after themselves."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("selfcare", SelfCare.message)
 
     @staticmethod
